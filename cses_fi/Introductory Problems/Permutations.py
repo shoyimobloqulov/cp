374b458a34fb6a83b2f844b739863ae6359087c0
Ma'murjon Mirzapo'latov Fazliddin o'g'li <mmdev2006@gmail.com>
2025-01-06 20:42:13 +0500

    Update Permutations.py

--- cses_fi/Introductory Problems/Permutations.py
+++ cses_fi/Introductory Problems/Permutations.py
@@ -1,1 +1,8 @@
-
+n = int(input())
+if n==3 or n==2:
+    print("NO SOLUTION")
+    quit()
+if n==4:
+    print(*[2,4,1,3])
+    quit()
+print(*[[i for i in range(1,n+1,2)]+[i for i in range(2,n+1,2)]][0])
